fa7c67a8c8726a1cbec322db02d51e13062b359c
taleclock <taleclock@live.com>
2015-11-15 20:59:09 -0800

    Have attack animation play on pressing f key.

--- Assets/Attack.cs
+++ Assets/Attack.cs
@@ -2,14 +2,16 @@
 using System.Collections;
 
 public class Attack : MonoBehaviour {
+    private Animator anim;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
+    // Use this for initialization
+    void Start () {
+				anim = GetComponentInChildren<Animator>();
+    }
+
 	// Update is called once per frame
 	void Update () {
-	
-	}
+        bool Attack = Input.GetButtonDown("Attack");
+        anim.SetBool("Attack", Attack);
+    }
 }
